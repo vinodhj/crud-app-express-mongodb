d32b008212617d0bf2bbb5088ac18efdc4e60fde
Vinodh <vinodh.jeevanantham@gmail.com>
2024-07-03 18:42:03 +0530

    curd done

--- index.js
+++ index.js
@@ -15,10 +15,66 @@ app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.post("/api/products", async function async(req, res) {
+// API - CRUD
+// Create
+app.post("/api/post-products", async function async(req, res) {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Read
+app.get("/api/products", async function async(req, res) {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Read by ID
+app.get("/api/product/:id", async function async(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Update
+app.put("/api/update-product/:id", async function async(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      res.status(404).json({ message: "Product not found" });
+    }
+    const updated_product = await Product.findById(id);
+    res.status(200).json(updated_product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Delete
+app.delete("/api/delete-product/:id", async function async(req, res) {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: err.message });
