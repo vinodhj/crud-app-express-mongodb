15e2df0e5cf8d5c204c640666e143683e3054b76
Vinodh <vinodh.jeevanantham@gmail.com>
2024-07-04 10:36:40 +0530

    route for products

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const Product = require("./models/product.model");
+const productRoute = require("./routes/product.route.js");
 dotenv.config();
 
 const app = express();
@@ -11,74 +11,12 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Routes
+app.use("/api", productRoute);
+
+// home route
 app.get("/", function (req, res) {
-  res.send("Hello World!");
-});
-
-// API - CRUD
-// Create
-app.post("/api/post-products", async function async(req, res) {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// Read
-app.get("/api/products", async function async(req, res) {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// Read by ID
-app.get("/api/product/:id", async function async(req, res) {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// Update
-app.put("/api/update-product/:id", async function async(req, res) {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      res.status(404).json({ message: "Product not found" });
-    }
-    const updated_product = await Product.findById(id);
-    res.status(200).json(updated_product);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// Delete
-app.delete("/api/delete-product/:id", async function async(req, res) {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      res.status(404).json({ message: "Product not found" });
-    }
-    res.status(200).json({ message: "Product deleted successfully" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ message: err.message });
-  }
+  res.send("NodeJS CRUD App");
 });
 
 // MongoDB connection
